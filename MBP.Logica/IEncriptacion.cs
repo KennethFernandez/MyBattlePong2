8fe4694a517adff5a5c14275667cce7da80df684
tavovar <tavo.var@hotmail.com>
2015-06-09 15:00:13 -0600

    DB
    Se agrega la base de datos

---
+++ MBP.Logica/IEncriptacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBP.Logica
+{
+    public interface IEncriptacion
+    {
+        public string encriptar(string contrasena);
+        public string desencriptar(string contrasena);
+    }
+}
