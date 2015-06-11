7ed35096775dba3c0f2051bb9f56eec2c34fab8d
rlopez0380 <rlopez0380@gmail.com>
2015-06-10 20:46:01 -0600

    nada
    nada

---
+++ MBP.EjeVertical/idPartidaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MBP.EjeVertical
+{
+    public class idPartidaModel
+    {
+        public int idPartida { get; set; }
+        public int idJugador { get; set; }
+    }
+}
